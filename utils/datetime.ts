79161968d8ec13f84e9d1124bcb02af28e66f800
NgBachCat <136461600+NgBachCat@users.noreply.github.com>
2025-01-25 18:59:55 +0700

    .

---
+++ utils/datetime.ts
@@ -0,0 +1,9 @@
+export function formatDateTime(date: Date): string {
+    return new Date(date).toLocaleString('vi-VN', {
+        hour: '2-digit',
+        minute: '2-digit',
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    });
+} 
